ae283f3948751111cb601eca6563939c90819faf
huangpw <1042850644@qq.com>
2025-02-12 07:30:24 +0800

    update

--- docs/.vitepress/config/sidebar.js
+++ docs/.vitepress/config/sidebar.js
@@ -9,9 +9,52 @@ module.exports = {
                 { text: 'CSS动画库', link: '/coding/common-framework/animate/' },
                 { text: 'CSS阴影库', link: '/coding/common-framework/shadow/' },
                 { text: 'CSS项目', link: '/coding/common-framework/project/' },
-                { text: 'CSS代码生成器', link: '/coding/common-framework/code-generate/' },
-                { text: 'Runtime API Examples', link: '/coding/common-framework/api-examples' }
+                { text: 'CSS代码生成器', link: '/coding/common-framework/code-generate/' }
             ]
-        }
-    ]
+        },
+        {
+            text: 'Vue生态',
+            collapsed: false,
+            items: [
+                { text: 'Vue全家桶', link: '/coding/common-framework/vue/' }
+            ]
+        },
+        {
+            text: 'React生态',
+            collapsed: false,
+            items: [
+                { text: 'React全家桶', link: '/coding/common-framework/vue/' }
+            ]
+        },
+        {
+            text: 'Node生态',
+            collapsed: false,
+            items: [
+                { text: 'Node框架', link: '/coding/common-framework/vue/' },
+                { text: '构建打包', link: '/coding/common-framework/vue/' }
+                
+            ]
+        },
+        {
+            text: '小程序生态',
+            collapsed: false,
+            items: [
+                { text: '小程序框架', link: '/coding/common-framework/vue/' }
+            ]
+        },
+        {
+            text: '跨平台生态',
+            collapsed: false,
+            items: [
+                { text: '跨平台框架', link: '/coding/common-framework/vue/' }
+            ]
+        },
+        {
+            text: '图标库',
+            collapsed: false,
+            items: [
+                { text: '图标库', link: '/coding/common-framework/vue/' }
+            ]
+        },
+    ],
 }
---
+++ docs/src/coding/common-framework/vue/data.js
@@ -0,0 +1,129 @@
+export const NAV_DATA = [
+  {
+    title: 'Vue官网',
+    items: [
+        {
+            icon: 'https://v2.cn.vuejs.org/images/logo.svg',
+            title: 'Vue2官网',
+            desc: '渐进式JavaScript框架。',
+            link: 'https://v2.cn.vuejs.org/',
+            badge: '官网'
+        },
+        {
+            icon: 'https://v2.cn.vuejs.org/images/logo.svg',
+            title: 'Vue3官网',
+            desc: '渐进式JavaScript框架。',
+            link: 'https://cn.vuejs.org/',
+            badge: '官网'
+        },
+        {
+            icon: './icons/nuxt.png',
+            title: 'Nuxt官网',
+            desc: 'Nuxt是一个基于Vue的框架，提供了服务端渲染（SSR）、静态生成（SSG）和PWA等功能，非常适合构建高性能的Web应用。',
+            link: 'https://nuxt.com/',
+            badge: '官网'
+        },
+        {
+            icon: 'https://vitejs.cn/vite3-cn/logo-with-shadow.png',
+            title: 'Vite官网',
+            desc: 'Vite是Vue3官方推荐的前端构建工具，以其快速的热更新和构建速度而闻名，极大提升了开发体验。',
+            link: 'https://vitejs.cn/vite3-cn/',
+            badge: '官网'
+        },
+        {
+            icon: './icons/pnpm.png',
+            title: 'pnpm',
+            desc: '在包管理器的选择上，首选 PNPM，相比于 NPM，它能解决很多问题，例如幽灵依赖、安装速度快、节省空间、天然支持Monorepo。',
+            link: 'https://www.pnpm.cn/',
+            badge: '官网'
+        },
+        {
+            icon: 'https://pinia.vuejs.org/logo.svg',
+            title: 'Pinia官网',
+            desc: 'Pinia是Vue3的官方状态管理库，它提供了一个直观、类型安全的状态管理方式，并且易于与TypeScript集成。',
+            link: 'https://pinia.vuejs.org/zh/',
+            badge: '官网'
+        },
+        {
+            icon: './icons/vuex.png',
+            title: 'Vuex',
+            desc: 'Pinia是Vue3的官方状态管理库，它提供了一个直观、类型安全的状态管理方式，并且易于与TypeScript集成。',
+            link: 'https://vuex.vuejs.org/zh/',
+            badge: '官网'
+        },
+        {
+            icon: 'https://router.vuejs.org/logo.svg',
+            title: 'Vue Router V4',
+            desc: 'Vue Router是Vue.js的官方路由管理器，它允许你构建单页面应用（SPA）。',
+            link: 'https://router.vuejs.org/zh/',
+            badge: '官网'
+        },
+        {
+            icon: 'https://router.vuejs.org/logo.svg',
+            title: 'Vue Router V3',
+            desc: 'Vue Router V4是官方的路由管理器，专为Vue3设计。它提供了一种声明式的方式来定义导航和页面路由，使得构建单页面应用（SPA）变得更加容易。',
+            link: 'https://v3.router.vuejs.org/zh/',
+            badge: '官网'
+        },
+    ],
+    },
+    {
+        title: '网络请求',
+        items: [
+            {
+                icon: './icons/axios.png',
+                title: 'Axios',
+                desc: 'Axios是一个基于promise的网络请求库，可以用于浏览器和node.js。',
+                link: 'https://www.axios-http.cn/',
+                badge: '官网'
+            },
+            {
+                icon: 'https://alova.js.org/zh-CN/img/logo-text.svg',
+                title: 'Alova',
+                desc: '一个流程简化的下一代请求工具，它可以将你的 API 集成工作流从 7 个步骤极致地简化为 1 个步骤，你只需要选择 API 即可使用。',
+                link: 'https://alova.js.org/zh-CN',
+                badge: '官网'
+            },
+            {
+                icon: './icons/fetch.png',
+                title: 'Fetch API',
+                desc: 'Fetch API是现代浏览器中内置的接口，用于执行网络请求。',
+                link: 'https://developer.mozilla.org/zh-CN/docs/Web/API/Fetch_API',
+                badge: '官网'
+            },
+        ]
+    },
+    {
+        title: 'Vue相关',
+        items: [
+            {
+                icon: './icons/typescript.png',
+                title: 'TypeScript',
+                desc: 'Vue3完全支持TypeScript，使得开发者可以在项目中享受到TypeScript提供的类型安全和开发效率。',
+                link: 'https://www.typescriptlang.org/',
+                badge: '官网'
+            },
+            {
+                icon: 'https://prazdevs.github.io/pinia-plugin-persistedstate/logo-light.svg',
+                title: 'Pinia Plugin Persistedstate',
+                desc: '一款Pinia数据持久化插件，可以用来对Store中的数据做本地持久化存储。',
+                link: 'https://prazdevs.github.io/pinia-plugin-persistedstate/zh/',
+                badge: 'Pinia'
+            },
+            {
+                icon: './icons/eslint.png',
+                title: 'ESLint',
+                desc: '查找和修复JavaScript代码中的问题。',
+                link: 'https://eslint.nodejs.cn/',
+                badge: '插件'
+            },
+            {
+                icon: 'https://prettier.io/icon.png',
+                title: 'Prettier',
+                desc: '一个支持多种编程语言和编辑器的代码格式化工具，可以按保存键时自动格式化代码，节省时间和精力。',
+                link: 'https://prettier.io/',
+                badge: '插件'
+            },
+        ]
+    }
+];
