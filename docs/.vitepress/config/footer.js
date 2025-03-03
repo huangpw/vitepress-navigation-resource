468a01633731e461984e7dc09ea3e7fc3f81ea17
huangpw <1042850644@qq.com>
2025-03-04 00:17:42 +0800

    update

--- docs/.vitepress/config/footer.js
+++ docs/.vitepress/config/footer.js
@@ -1,6 +1,6 @@
 module.exports = {
   message: 'Released under the MIT License.如有转载或 CV 的请标注本站原文地址',
   // message: `<a href="https://beian.miit.gov.cn/" target="_blank">京ICP备2001661114号-2</a>`,
-  copyright: 'Copyright © 2025-present HUANGPW',
+  copyright: 'Copyright © 2025-present Huangpw',
   // Copyright © 2021-2023 www.layui-vue.com
 };
